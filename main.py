8c99a8fd236e96478cc59660688e327aa0bfacda
ryanflynn9299 <ryan.flynn001@gmail.com>
2022-05-23 09:51:01 -0700

    imports and basic tff hello world

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ CSC 325-03 Project 1
 
 """
 
+import collections
 import nest_asyncio
 import tensorflow as tf
 import tensorflow_federated as tff
@@ -13,7 +14,8 @@ def main():
     np.random.seed(0)
     nest_asyncio.apply()
 
-    tff.federated_computation(lambda: 'Hello, World!')()
+    federated_result = tff.federated_computation(lambda: 'Hello, World!')()
+    print(federated_result)
 
 
 if __name__ == '__main__':
